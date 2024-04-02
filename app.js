199e457a9c5ae7615ef75632afae128f2155f088
Pratyush Sarkar <134036745+pratyush1969@users.noreply.github.com>
2024-04-02 19:55:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,29 +4,18 @@ const celcius = document.querySelector("#celcius")
 const heading = document.querySelector("h2");
 const hum = document.querySelector("#hum")
 const air = document.querySelector("#air")
-
+const pic=document.querySelector(".pic")
 async function weather(city) {
-    const api_key = "dd551dd3c95de517caeb65e5ede12b0a"
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`
+    const API_KEY = "e0738db10be6671e3e1e622bd0f30dfa"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
     const data = await fetch(`${url}`).then(response => response.json());
    console.log(data)
+   celcius.innerHTML=`${Math.round(data.main.temp-273.15)}°C`;
+   heading.innerHTML=`${data.weather[0].description}`
+   hum.innerHTML=`${data.main.humidity}`
+   air.innerHTML=`${data.wind.speed}`
+   pic.innerHTML=`<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">`
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-btn.addEventListener("click", () => {
+btn.addEventListener("click", function(){
     weather(input.value);
 })
-
